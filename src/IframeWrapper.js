8b0da7fba56efd697d0609fa8d2211359af5d805
mkotsollaris <mkotsollari@gmail.com>
2020-07-18 22:52:29 -0700

    added IframeWrapper.js

---
+++ src/IframeWrapper.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function({inputURL}) {
+    return <div>
+        <iframe title="website-loader" width="820rem" height="820rem" src={inputURL}></iframe>
+    </div>
+}
