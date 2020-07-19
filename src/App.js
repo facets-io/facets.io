e09126838851be5b33eccbd174d40700fdc42b10
mkotsollaris <mkotsollari@gmail.com>
2020-07-18 21:42:28 -0700

    added bootstrap-switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 function App() {
+  const [inputURL, setInputURL] = useState('https://stripe.com/');
+  const [editMode, setEditMode] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledDiv>
+      <StyledLabel>Load Website URL:</StyledLabel>
+      <StyledInput value={inputURL} onChange={e => setInputURL(e.target.value)} type="text" name="name" />
+      <StyledSwitch
+          checked={false}
+          onlabel='Edit'
+          onstyle='danger'
+          offlabel='Live'
+          offstyle='success'
+          size='sm'
+          onChange={(checked) => {
+            setEditMode(!editMode);
+          }}
+        />
+      </StyledDiv>
+      <StyledDiv>
+        
+      </StyledDiv>
+      <iframe title="website-loader" width="820rem" height="820rem" src={inputURL}></iframe>
     </div>
   );
 }
+
+const StyledSwitch = styled(BootstrapSwitchButton)`
+  margin: 1rem;
+`;
+
+const StyledInput = styled.input`
+  margin: 1rem;
+`;
+
+const StyledDiv = styled.div`
+  margin: 2rem;
+`;
+
+const StyledLabel = styled.label`
+  margin: 2rem;
+`;
 
 export default App;
---
+++ src/toolbox/ToolBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ToolBox() {
+    return <div></div>
+}
+
+export default ToolBox;
