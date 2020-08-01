fec36fae54fe7847e5f44d9afe44ab78e13a63fb
mkotsollaris <mkotsollari@gmail.com>
2020-08-01 10:44:42 -0700

    added highlight

---
+++ src/highlight.js
@@ -0,0 +1,34 @@
+var a = document.getElementsByTagName('*')
+for (i in a) {
+    a[i].onmouseover = function (e) {
+        // console.log('mpika man');
+        // this.css('border', '1px solid black');
+
+        this.style.border = '1px solid black';
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    };
+    a[i].onmouseout = function (e) {
+        // this.css('border', 'none');
+        this.style.border = 'none';
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    };
+}
+
+// $('*').hover(
+//     function (e) {
+//         console.log('mpika man');
+//         $(this).css('border', '1px solid black');
+//         e.preventDefault();
+//         e.stopPropagation();
+//         return false;
+//     }, function (e) {
+//         $(this).css('border', 'none');
+//         e.preventDefault();
+//         e.stopPropagation();
+//         return false;
+//     }
+// );
