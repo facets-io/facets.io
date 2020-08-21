7db3623fea22dd31747969a65e3cb335fab6d8eb
mkotsollaris <mkotsollari@gmail.com>
2020-08-21 09:12:34 -0700

    added images

--- src/index.js
+++ src/index.js
@@ -6,28 +6,28 @@ import * as serviceWorker from './serviceWorker';
 import AppProvider from './AppProvider';
 import { SnackbarProvider } from "notistack";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <SnackbarProvider maxSnack={4}
-      disableWindowBlurListener
-      autoHideDuration={5000}
-      iconVariant={{
-        success: '⚔️',
-        error: '✖️',
-        warning: '⚠️',
-        info: 'ℹ️',
-      }}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}>
-      <AppProvider >
-        <App />
-      </AppProvider>
-    </SnackbarProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <SnackbarProvider maxSnack={4}
+//       disableWindowBlurListener
+//       autoHideDuration={5000}
+//       iconVariant={{
+//         success: '⚔️',
+//         error: '✖️',
+//         warning: '⚠️',
+//         info: 'ℹ️',
+//       }}
+//       anchorOrigin={{
+//         vertical: 'bottom',
+//         horizontal: 'right',
+//       }}>
+//       <AppProvider >
+//         <App />
+//       </AppProvider>
+//     </SnackbarProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
