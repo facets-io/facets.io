0450db1d6d714052f361df85be93a41ee561bf9d
andy <andrew.w.pinson@gmail.com>
2020-09-17 17:27:43 -0700

    Changing marketing video.

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -112,7 +112,7 @@ const Subscribe = (props) => {
             </form>
         </StyledDiv>
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/E85GKyn-PNA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ybPR_bpp2OM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
 }
