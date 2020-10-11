ddc6a67bac44fd9479f73dbe703345fec01a8f31
mkotsollaris <mkotsollari@gmail.com>
2020-10-11 15:38:39 -0700

    updated vision video url

--- src/IframeWrapper.js
+++ src/IframeWrapper.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function () {
     return <div>
-        <iframe title='vision' width="560" height="315" src="https://www.youtube.com/embed/zr6Ul_1DU3I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/K1UDhr1uJL0"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen="allowfullscreen"
+            mozallowfullscreen="mozallowfullscreen"
+            msallowfullscreen="msallowfullscreen"
+            oallowfullscreen="oallowfullscreen"
+            webkitallowfullscreen="webkitallowfullscreen"></iframe>
     </div>
 }
