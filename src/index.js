20e2873e6205972844d22127e63a16a7e290b0a7
mkotsollaris <mkotsollari@gmail.com>
2020-10-11 18:38:44 -0700

    added sentry integration

--- src/index.js
+++ src/index.js
@@ -12,7 +12,18 @@ import {
 } from "react-router-dom";
 import Policy from './Policy';
 import IframeWrapper from './IframeWrapper';
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: 'https://84b50fd8397241d3a0976c6e23a5ce0c@o460218.ingest.sentry.io/5460088',
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
