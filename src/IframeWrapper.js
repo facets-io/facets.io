4e0595ac0059935d6c9c5c87d845c6e00ef404dd
mkotsollaris <mkotsollari@gmail.com>
2020-10-28 11:14:15 -0700

    reddit live

--- src/IframeWrapper.js
+++ src/IframeWrapper.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Docs from './docs/Docs'
 import styled from 'styled-components';
-import facetNinjaDLA from './images/facet-ninja-dla.jpg';
-
-const StyledIframe = styled.iframe`
-    margin-top: 1rem;
-`;
+import facetNinjaDLA from './images/fn-dla.png';
+import redditLive from './images/reddit-live.jpg';
 
 const StyledImg = styled.img`
     margin-top: 1rem;
-    width: 30%;
+    width: 70%;
 `
+
+const TopImgDiv = styled.div`
+    display: grid;
+    grid-template-columns: 55% 45%;
+    justify-items: center;
+    align-items: center;
+    margin-bottom: 2rem;
+`;
 
 export default function () {
     return <div>
-        <StyledImg src={facetNinjaDLA} />
+        <TopImgDiv>
+            <div><a href='https://facet.ninja/'><StyledImg src={facetNinjaDLA} /></a></div>
+            <div><a><StyledImg src={redditLive} /></a></div>
+        </TopImgDiv>
         <Docs />
     </div>
 }
--- src/docs/Docs.js
+++ src/docs/Docs.js
@@ -18,10 +18,13 @@ const StyledDiv = styled.div`
 const LRMarginDiv = styled.div`
     margin-right: 2rem;
     margin-left: 2rem;
+    margin-bottom: 5rem;
 `;
 
 const CenteredDiv = styled.div`
     text-align: center;
+    margin-left: 5%;
+    margin-right: 5%;
 `;
 
 const StyledBorder = styled.div`
@@ -40,11 +43,44 @@ export default function () {
     return <StyledDiv>
         <LRMarginDiv>
             <CenteredDiv>
-                <h1>Hello Hackernews! 👋</h1>
-                <h2> We are facet ninja and we are on a mission to streamline software delivery! Below you will find some information on how
-            to get started with facet-extension. Should you have any question, please feel free to either contact through <a href='https://spectrum.chat/facet'>spectrum.chat </a>
-             or open <a href='https://github.com/facets-io/facet-extension/issues'> a github issue</a>! <br /> <br /> THANK you again for checking us out 🙏 🚀 📦</h2>
+                <h1>Hello Reddit 😻 👋 !</h1>
+                <h2> We are facet ninja and we are on a mission to streamline software delivery!</h2>
+                <h3>❓ mmm 🤔 okay... but what do you do ❓ </h3>
+                <h3>We do low-code rollouts (traditionally achieved via API/SDK feature flags) that power engineers and managers to ship feature faster!</h3>
+                <h3>We got burnout by</h3>
+                <h3>❓  And Why is this important ❓ </h3>
+                <h3>We are devs ourselves and we have spent too much time on maintaining vendors and useless code that goes in and out manually with feature flags.
+                We want to power engineers and devops to rollout continuously without any code, so devs like us can focus on the important stuff!</h3>
+                <h2> Below you will find some information on how to get started with facet-extension. Should you have any question, please feel free to either contact through
+                <a href='https://spectrum.chat/facet'> spectrum.chat </a> or open <a href='https://github.com/facets-io/facet-extension/issues'> a github issue</a>.</h2>
+                <h2>
+                    We are open-source and we welcome <a href='https://github.com/facets-io/facet-extension/blob/master/CONTRIBUTING.md'>contributions</a>!</h2>
+                <h2>
+                    <br /> THANK you again for checking us out 🙏 🚀 📦</h2>
             </CenteredDiv>
+            <div>
+                <h1>Videos & tutorials 📹:</h1>
+                <br />
+                <br />
+                <CenteredDiv>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/WARSSkFQHKg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <br />
+
+                    <br />
+                    <br />
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/K1UDhr1uJL0"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen="allowfullscreen"
+                        mozallowfullscreen="mozallowfullscreen"
+                        msallowfullscreen="msallowfullscreen"
+                        oallowfullscreen="oallowfullscreen"
+                        webkitallowfullscreen="webkitallowfullscreen"></iframe>
+                </CenteredDiv>
+            </div>
             <h2>How to use guide:</h2>
             <LRMarginDiv>
                 1. Install the plugin via <a href='https://chrome.google.com/webstore/detail/facetninja/hpkpjkdhgldjhcopdkmljdgceeojoglh'>Chrome Web Store</a>.
@@ -86,27 +122,6 @@ export default function () {
                 <br />
                 5. Paste the snippet to your application and see the results! 🚀
             </LRMarginDiv>
-            <h1>Some more videos 📹</h1>
-            <br />
-            <br />
-            <CenteredDiv>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/WARSSkFQHKg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <br />
-
-                <br />
-                <br />
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/K1UDhr1uJL0"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen="allowfullscreen"
-                    mozallowfullscreen="mozallowfullscreen"
-                    msallowfullscreen="msallowfullscreen"
-                    oallowfullscreen="oallowfullscreen"
-                    webkitallowfullscreen="webkitallowfullscreen"></iframe>
-            </CenteredDiv>
         </LRMarginDiv>
     </StyledDiv>
 }
