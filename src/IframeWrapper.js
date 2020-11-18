464aa623e452b2b18de855c84096e10cb243a49d
mkotsollaris <mkotsollari@gmail.com>
2020-11-18 00:18:07 -0800

    updated video

--- src/IframeWrapper.js
+++ src/IframeWrapper.js
@@ -2,16 +2,6 @@ import React from 'react';
 
 export default function () {
     return <div>
-        <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/K1UDhr1uJL0"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen="allowfullscreen"
-            mozallowfullscreen="mozallowfullscreen"
-            msallowfullscreen="msallowfullscreen"
-            oallowfullscreen="oallowfullscreen"
-            webkitallowfullscreen="webkitallowfullscreen"></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/WARSSkFQHKg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 }
