ea764169a35ba928f85d6cbf61d0f659d0fe679d
mkotsollaris <mkotsollari@gmail.com>
2021-01-04 18:13:37 -0800

    attempt to fix build

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/documentation': { page: '/documentation' },
+            '/pricing': { page: '/pricing' },
+            '/blog': { page: '/blog' },
+        }
+    },
+}
