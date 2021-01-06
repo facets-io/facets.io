704996abe3a6f7ba15af0a4ccffa0897e5757fc0
mkotsollaris <mkotsollari@gmail.com>
2021-01-05 22:55:31 -0800

    added rhf in yarn

---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    display: grid;
+    text-align: center
+`
+
+export default function ContactPage() {
+    return (<>
+        <StyledDiv>
+            <h3 style={{ color: 'white' }}>Thanks for checking us out! Our blog is currently under construction. <br />🚧 Coming soon!</h3>
+            <h3 style={{ color: 'white' }}> </h3>
+        </StyledDiv>
+    </>
+    )
+}
