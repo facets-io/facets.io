2e16a597a4db80b6220de69e0ff06d7d6f510580
mkotsollaris <mkotsollari@gmail.com>
2021-01-06 14:48:41 -0800

    save progress

--- shared/components/ContactGrid.tsx
+++ shared/components/ContactGrid.tsx
@@ -1,11 +1,16 @@
+import { TextField } from '@material-ui/core'
 import styled from 'styled-components'
 import { color } from '../constant'
+import FacetButton, { electricBtnColor } from './FacetButton'
 import FacetInput from './FacetInput'
 import FacetLabel from './FacetLabel'
 import RadioButtons from './RadioButtons'
+import StayUpdated from './StayUpdated'
 
 const StyledDiv = styled.div`
     display: grid;
+    margin-left: 2rem;
+    margin-right: 2rem;
 `
 
 const StyledForm = styled.form`
@@ -26,6 +31,7 @@ export default function ContactGrid() {
             <p>We'd love to hear from you! Contact us using the form below and we'll be happy to help answer your questions.</p>
             <h4>Get in touch</h4>
             <StyledForm onSubmit={(e) => e.preventDefault()}>
+                <br />
                 <TwoColumnGrid>
                     <div>
                         <FacetLabel text="First name" />
@@ -52,10 +58,35 @@ export default function ContactGrid() {
                     <div>
                         <FacetLabel text="Do you currently use Facet?" />
                         <br />
+                        <br />
                         <RadioButtons />
                     </div>
                 </TwoColumnGrid>
+                <br />
+                <div style={{}}></div>
+                <div style={{ padding: '1rem' }}>
+                    <FacetLabel text="Message" />
+                    <br />
+                    <br />
+                    <TextField
+                        style={{ backgroundColor: color.ice }}
+                        id="outlined-multiline-static"
+                        multiline
+                        rows={7}
+                        fullWidth
+                        placeholder="Enter your message"
+                        variant="outlined"
+                    />
+                    <br />
+                    <br />
+                    <FacetButton colorButtonStyle={electricBtnColor} style={{ width: '20%' }} text="Submit" />
+                    <br />
+                </div>
             </StyledForm>
+            <br />
+            <br />
+            <StayUpdated />
+            <br />
         </StyledDiv>
     )
 }
--- shared/components/StayUpdated.tsx
+++ shared/components/StayUpdated.tsx
@@ -1,10 +1,14 @@
-import FacetButton from "./FacetButton";
+import FacetButton, { electricBtnColor } from "./FacetButton";
 import FacetInput from "./FacetInput";
 import styled from 'styled-components';
 
 const MainDiv = styled.div`
     width: 100%;
     text-align: center;
+    display: grid;
+    grid-template-columns: 15rem 15rem;
+    grid-gap: 5%;
+    justify-content: center;
 `;
 
 export default function StayUpdated() {
@@ -14,7 +18,7 @@ export default function StayUpdated() {
                 <FacetInput placeholder="email address" />
             </div>
             <div>
-                <FacetButton text="STAY UPDATED" />
+                <FacetButton colorButtonStyle={electricBtnColor} text="STAY UPDATED" />
             </div>
         </MainDiv>
     );
