810a10cf9719d1b7858b111ac2034bd113e45ed0
mkotsollaris <mkotsollari@gmail.com>
2021-01-07 19:15:02 -0800

    added PricingCalculator

---
+++ shared/components/PricingCalculator.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+    display: grid;
+    height: 100%;
+`
+
+export default function PricingCalculator() {
+    return (
+        <>
+        </>
+    )
+}
