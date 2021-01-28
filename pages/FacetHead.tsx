9097a7f7d0a6ce1857351110835f79faaa21c918
mkotsollaris <mkotsollari@gmail.com>
2021-01-28 09:52:21 -0800

    updated facet script to facet.run

--- pages/FacetHead.tsx
+++ pages/FacetHead.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default () => {
     return <Head>
-        <script src="https://api.facet.ninja/facet.ninja.js?id=DOMAIN~MGU3YjI1NDAtN2I2Yy00M2M0LWE2NDctNTk1ODdlYjlkYjQ5"></script>
+        <script src="https://api.facet.run/js?id=DOMAIN~MGU3YjI1NDAtN2I2Yy00M2M0LWE2NDctNTk1ODdlYjlkYjQ5"></script>
         <title>Facet</title>
         <link rel="icon" href="/favicon.ico" />
     </Head>
