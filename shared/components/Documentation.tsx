feda80d8fdb74a0b348312bfa707c3e30369c822
mkotsollaris <mkotsollari@gmail.com>
2021-01-31 20:38:07 -0800

    updated text

--- shared/components/Documentation.tsx
+++ shared/components/Documentation.tsx
@@ -30,14 +30,16 @@ export default () => {
             <div>
                 <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.facetTitle}>Introduction</h1>
                 <FacetParagraph color={"#5D5D5D"}>
-                    Facet allows product teams to rollout features without engaging engineering resources. In this step by step guide, we will showcase how to
-                    install the facet-extension, create facets, inject the Facet CDN script and finally rollout/rollback desired features in production.
+                    Facet allows product teams to rollout features without engaging engineering resources. In this guide, we describe how to install the facet extension, create facets, inject the facet CDN script, and rollout/rollback desired features in production.
+                    <br />
+                    <br />
+                    A facet is a group of elements that make up a feature. For instance, a login form could be considered a facet with the elements being “username” and “password”.
                 </FacetParagraph>
                 <br />
                 <FacetH1 text="Download and install the facet-extension" id={documentationIds.download} />
                 <FacetParagraph color={"#5D5D5D"}>
-                    You can find the latest version of the facet-extension through <a href="http://cdn.facet.run/extension-chrome-facet-ninja.zip">this URL</a>. Once the zip is downloaded, unzip the folder and load it in the
-                    Chrome Web store via the "Load unpacked" option. "Manage Extensions" {'->'} "Load unpacked" {'->'} "Load the unzipped folder". The extension should then be loaded in your browser.
+                    You can find the latest version of the facet-extension through <a href="http://cdn.facet.run/extension-chrome-facet-ninja.zip">this URL</a>. Unzip the folder and load it in the Chrome Web Store via the "Load unpacked" option. Click on “Manage Extensions” in the Chrome browser and enable “Developer mode“ on the top right corner. On the top left corner, select "Load unpacked"
+                     {'->'} "Load the unzipped folder". The extension should then be loaded in your browser.
                     <br />
                     <br />
                     <div style={{ textAlign: 'center' }}>
@@ -47,8 +49,10 @@ export default () => {
                         </figure>
                     </div>
                     <FacetBlockQuote>
-                        We are actively working on publishing the extension through the chrome web store, and once we do, we will update the docs accordingly.
+                        We are actively working on publishing the extension through the Chrome Web Store. Once it is published, we will update the documentation accordingly.
                     </FacetBlockQuote>
+                    <br />
+                    After the installation, create and authenticate your user account. The facet extension is now ready for use.
                 </FacetParagraph>
                 <br />
                 <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.addDomainToWorkspace}>Add Domain to Workspace</h1>
@@ -64,13 +68,17 @@ export default () => {
                 </div>
                 <br />
                 <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.facetDeclaration}>Declare facets</h1>
-                <FacetParagraph color={"#5D5D5D"} text="A facet is a set elements that construe a feature. For instance, a login form could be considered a facet, with the elements being username and password." />
+                <FacetParagraph color={"#5D5D5D"}>
+                    Creating/Reading/Updating/Deleting (CRUD) facets is achieved by using the facet extension. To start using facets, log into the platform. Navigate to the application where you want to create facets, and click on the facet extension icon on the top right corner of your browser. Click on “Whitelist” to enable the extension on this domain.
+                </FacetParagraph>
                 <br />
                 <br />
-                <FacetParagraph color={"#5D5D5D"} text="Creating/Reading/Updating/Deleting (CRUD) facets is achieved by using the facet chrome extension. After you install the extension from the Chrome Web Store, you can login into the platform.  Navigate to the application where you want to create facets, and enable the plugin through the popup." />.
-                <br />
-                <br />
-                <FacetParagraph color={"#5D5D5D"} text="A sidebar will be loaded on the left side, which will offer the ability of CRUD-ing facets. The facet-extension can be enabled through the very first button on the top navigation bar.  Declaring a facet is as easy as clicking on the element of interest, as shown below:" />
+                <FacetParagraph color={"#5D5D5D"}>
+                    The user interface of the facet extension is a sidebar that loads on the left. Start creating facets by clicking on the leftmost button on the top navigation bar of the extension. Click on the elements of interest to group them under your facet:
+                    <br />
+                    <br />
+                    You can rename and delete a facet by clicking on [3 dots]. To create a new facet, click on the + [show it] button at the bottom right corner of the extension.
+                </FacetParagraph>
                 <br />
                 <div style={{ textAlign: 'center' }}>
                     <figure>
@@ -81,15 +89,19 @@ export default () => {
                 <br />
                 <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.preview}>Preview</h1>
                 <FacetParagraph color={"#5D5D5D"} >
-                    Preview allows you to see how your integration would work without having to integrate. By clicking on the "Save & Preview" button, you will
-                    be able to see how the website would look like in production, without having to copy paste the snippet in your DOM.
+                    By clicking on the display/hide icon next to each facet, you are able to rollout facets in production. Once your configuration is ready, save your changes and your configuration will be applied into your application.
+                    <br />
+                    <br />
+                    Preview the facets you have enabled by clicking on [preview]. A new tab opens, previewing your current configuration.
+                    <br />
+                    <br />
+                    Save or delete your configuration by using the <b>delete</b> or <b>save</b> buttons on the top navigation bar.
                 </FacetParagraph>
                 <br />
                 <br />
-                <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.oneLineCodeIntegration}>Live Integration</h1>
+                <h1 style={{ fontFamily: "Helvetica", fontSize: "27px", fontWeight: 600 }} id={documentationIds.oneLineCodeIntegration}>Integration</h1>
                 <FacetParagraph color={"#5D5D5D"}  >
-                    Once your configuration is ready, save your changes and your configuration will be applied into your application. Then copy paste the snippet of code into
-                    your head html element.
+                    Copy the snippet of code generated by the facet extension and displayed at the top of the extension. This will give you the single line of code that you need to paste into your head HTML code. The ID refers to the ID of your website. Once this line of code is integrated in your system, you should be able to see the configurations applied from the facet extension.
                     <pre><code>
                         {preCodeValue}
                     </code></pre>
