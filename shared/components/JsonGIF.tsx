2372bc6cfee7bacef86e2161c49654ca12dbbb76
mkotsollaris <mkotsollari@gmail.com>
2021-02-08 13:58:10 -0800

    updated JSONGif component

--- shared/components/JsonGIF.tsx
+++ shared/components/JsonGIF.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Lottie from 'react-lottie';
 
-export default function ({animationData, isStopped= false, isPaused= false, style= {
-            overflow:"hidden",
-            width: undefined,
-            height: undefined,
-            boxShadow: undefined
-        },
-        options = {
-            loop: true,
-            autoplay: true,
-            rendererSettings: {
-                preserveAspectRatio: 'xMidYMid slice'
+export default function ({ animationData, isStopped = false, isPaused = false, style = {
+    borderRadius: undefined,
+    overflow: "hidden",
+    width: undefined,
+    height: undefined,
+    boxShadow: undefined
+},
+    options = {
+        loop: true,
+        autoplay: true,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
         }
-    }}
+    } }
 ) {
     const defaultOptions = {
         ...options, animationData
@@ -21,8 +22,8 @@ export default function ({animationData, isStopped= false, isPaused= false, styl
 
     return <div style={style}>
         <Lottie options={defaultOptions}
-                isStopped={isStopped}
-                isPaused={isPaused}/>
+            isStopped={isStopped}
+            isPaused={isPaused} />
     </div>
 
 }
