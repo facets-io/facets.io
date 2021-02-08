7ece92884b1c61a1bf0f6f8a5810683f16a4e6fc
mkotsollaris <mkotsollari@gmail.com>
2021-02-08 13:18:50 -0800

    added google analytics

--- pages/FacetHead.tsx
+++ pages/FacetHead.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head'
 export default () => {
     return <Head>
         <script src="https://api.facet.run/js?id=DOMAIN~ZmIzYzIzMTYtY2ZhYi00ZjY3LWFhOGQtOGM5YmVjMDdlMWQ4"></script>
+        {/* google analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3V7FRFN53H"></script>
+        <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-3V7FRFN53H');
+              `,
+        }}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Facet</title>
         <link rel="icon" href="/favicon.ico" />
-    </Head>
+    </Head >
 }
