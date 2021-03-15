4991e55529286a2b4b7ff0db37f8bcfc3c8f229c
mkotsollaris <mkotsollari@gmail.com>
2021-03-15 15:55:51 -0700

    cleanup

--- shared/components/Documentation.tsx
+++ shared/components/Documentation.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { color } from '../constant';
 import FAQ from './FAQ';
 import Introduction from './Documentation/Introduction';
-import DownloadAndInstall from './Documentation/DownloadAndInstall';
 import AddDomain from './Documentation/AddDomain';
 import FacetDeclaration from './Documentation/FacetDeclaration';
 import Preview from './Documentation/Preview';
@@ -27,7 +26,6 @@ const Documentation = () => {
             <div>
                 <Introduction />
                 {Space}
-                <DownloadAndInstall />
                 <InstallChromeStore />
                 {Space}
                 <AddDomain />
