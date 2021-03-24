d22dbbce63d74c8111315a9f46573a90c8e02db7
mkotsollaris <mkotsollari@gmail.com>
2021-03-24 16:11:32 -0700

    updated JavaVerify.tsx

--- shared/components/JavaVerify.tsx
+++ shared/components/JavaVerify.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { backendDocumentationIds } from '../constant';
 import FacetH1 from './FacetH1';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+const YMLCode = () => {
+    const facetYmlString = `workspaceId: WORKSPACE~ID
+name: My-Application
+environment: dev`
+    return (
+        <SyntaxHighlighter showLineNumbers language="language-markup" style={atomDark}>
+            {facetYmlString}
+        </SyntaxHighlighter>
+    );
+}
 
 const JavaVerify = () => {
     return <div>
         <FacetH1 id={backendDocumentationIds.verify}>Verify</FacetH1>
-        Navigate into the <a href='https://app.facet.run' target='_blank'>dashboard</a>. Right after you login, select "Java". You should be able to see
+        Create a <i>facet.yml</i> file in your project directory. The file contains your <i>workspaceId</i>, your project's name and your environment.
+        You can retrieve your workspaceId by login in into the dashboard.
+        <YMLCode />
+        Navigate into the <a href='https://app.facet.run' target='_blank'>dashboard</a>. Right after you login, select "Applications" -> "My-Application". You should be able to see
         all the live methods and endpoints, alongside with a checkbox allowing their enablement and disablement.
         <div style={{ textAlign: 'center' }}>
             <figure>
