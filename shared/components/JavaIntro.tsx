1bbb930bfb0df8eb0d37b48588406ae7e9166f71
Menelaos Kotsollaris <mkotsollaris@Menelaoss-MacBook-Pro.local>
2021-04-07 21:42:55 -0700

    update JavaIntro

--- shared/components/JavaIntro.tsx
+++ shared/components/JavaIntro.tsx
@@ -7,8 +7,8 @@ import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const MapCodeBlock = () => {
     const codeString = `    void    -> void
-    byte    -> Byte.MIN_VALUE
-    short   -> Short.MIN_VALUE
+    byte    ->  Byte.MIN_VALUE
+    short   ->  Short.MIN_VALUE
     int     -> Integer.MIN_VALUE
     long    -> Long.MIN_VALUE
     float   -> Float.MIN_VALUE
@@ -28,11 +28,10 @@ const JavaIntro = () => {
     return <div>
         <FacetH1 id={backendDocumentationIds.introduction}>Introduction</FacetH1>
         <FacetParagraph>
-            The Facet Java SDK can be used with Kotlin, Scala, and other JVM languages. Java-Agent allows <i>enabling</i> and <i>disabling</i> methods and endpoints in your application.
+            The Facet Java SDK can be used with the Spring Framework. Java-Agent allows <i>enabling</i> and <i>disabling</i> methods and endpoints in your application.
             When a method is disabled, a default value is returned, which is usually the minimum value of the class. This is how values are mapped:
             <MapCodeBlock />
-            Code examples are typically provided in both Java and Kotlin.
-            On this page, we get you up and running with Facet's SDK, so that it will automatically switch methods and endpoints to enable/disable.
+            On this page; get up and running with Facet's SDK, so that it will automatically switch methods and endpoints to enable/disable.
         </FacetParagraph>
     </div>
 }
