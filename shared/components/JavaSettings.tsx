630749b663ac1ccc8880361f1a5ce30199fa6dba
Menelaos Kotsollaris <mkotsollaris@Menelaoss-MacBook-Pro.local>
2021-04-08 00:09:16 -0700

    updated blocklist_settings.jpg

--- shared/components/JavaSettings.tsx
+++ shared/components/JavaSettings.tsx
@@ -12,7 +12,7 @@ const JavaSettings = () => {
             For instance, ignoring <i>com.sun</i> package.
         </FacetParagraph>
         <div style={{ textAlign: 'center' }}>
-            <FacetLightboxImage src='../../images/blocklist_settings.png' captionText='Updating block list' />
+            <FacetLightboxImage src='../../images/blocklist_settings.jpg' captionText='Updating block list' />
         </div>
         <FacetParagraph>
             Note that adding packages requires passing <i>"/" </i> as a delimiter. For instance, for adding <i>com.sun</i> would be defined as <i>com/sun</i>.
